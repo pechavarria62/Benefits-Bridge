c3b66297f2f614ca79a950fbc8f6d6823b7fa974
Pedro Echavarria <pechavarria62@gmail.com>
2026-01-26 11:32:01 -0500

    making some adjustments

--- src/pages/ZipCode.jsx
+++ src/pages/ZipCode.jsx
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import benefitsData from "../lib/benefits.json";
-import Iconz from '../components/IconMapper';
+import Iconz from "../components/IconMapper";
 
 function ZipCode() {
- const [formData, setFormData] = useState({
-  zip: "",
-  showForm: false,
-  selectedBenefits: [],
-  jobDetails: {
-    employment: "Full-time ",
-    hours: 40,
-    payType: "Hourly ",
-    payRate: "Weekly ",
-  },
-});
-const [benefits, setBenefits] = useState([]);
+  const [formData, setFormData] = useState({
+    zip: "",
+    showForm: false,
+    selectedBenefits: [],
+    jobDetails: {
+      employment: "Full-time ",
+      hours: 40,
+      payType: "Hourly ",
+      payRate: "Weekly ",
+    },
+  });
+  const [benefits, setBenefits] = useState([]);
 
-// Load benefits data when component mounts
+  // Load benefits data when component mounts
   useEffect(() => {
     setBenefits(benefitsData.benefits);
   }, []);
 
   // Called when the user clicks "Continue" after entering ZIP
   const handleContinue = () => {
-    const zip = formData.zip.trim(); 
+    const zip = formData.zip.trim();
     //Zip has to be integers and 5 digitsonly
     const CorrectZip = /^\d{5}$/.test(zip);
 
@@ -32,7 +32,6 @@ const [benefits, setBenefits] = useState([]);
     } else {
       alert("Zip code has to be numbers and 5 digits only please.");
     }
-
   };
 
   // Toggle a benefit on/off in selectedBenefits
@@ -46,17 +45,19 @@ const [benefits, setBenefits] = useState([]);
   };
 
   // Update jobDetails fields when user changes inputs
- const handleJobChange = (field, value) => {
+  const handleJobChange = (field, value) => {
     setFormData((prev) => ({
       ...prev,
       jobDetails: { ...prev.jobDetails, [field]: value },
     }));
   };
   return (
-     <div className="flex flex-col items-center justify-center h-screen min-h-screen p-4">
+    <div className="flex flex-col items-center justify-center h-screen min-h-screen p-4">
       {!formData.showForm ? (
         <>
-          <h1 className="text-4xl font-bold text-[#5664f5] mb-2">Welcome to Job</h1>
+          <h1 className="text-4xl font-bold text-[#5664f5] mb-2">
+            Welcome to Job
+          </h1>
           <p className="text-gray-700 max-w-lg mb-4 leading-normal">
             Understand how taking a new job can affect your government benefits.
             Enter your ZIP code to get started.
@@ -76,36 +77,42 @@ const [benefits, setBenefits] = useState([]);
                 border-solid border focus:border-purple-500
                 focus:outline-none
                 focus:text-[#5664f5]"
-              />
-              <button onClick={handleContinue} className="bg-[#5664f5] text-white
+            />
+            <button
+              onClick={handleContinue}
+              className="bg-[#5664f5] text-white
                 border-none rounded-md
                 py-2 px-4 text-base font-semibold cursor-pointer
                 hover:bg-purple-600 focus:outline-none "
-                >
-                Continue
-              </button>
+            >
+              Continue
+            </button>
           </div>
         </>
       ) : (
         <>
-          <h2 className="text-5xl font-bold text-black mb-2 flex flex-col">Select Benefits</h2>
-          <ul className=" text-[#5664f5] px-4 w-72 ">
+          <h2 className="text-5xl font-bold text-black mb-2 flex flex-col">
+            Select Benefits
+          </h2>
+          <ul className=" text-[#5664f5] px-2 w-72 ">
             {benefits.map((benefit) => (
               <li key={benefit.id} className="w-full">
                 <label className="flex items-center space-x-4 mb-2 cursor-pointer">
                   <input
                     type="checkbox"
-                    className=''
+                    className=""
                     checked={formData.selectedBenefits.includes(benefit.id)}
                     onChange={() => toggleBenefit(benefit.id)}
                   />
-                  <span className="text-[#5664f5] text-2xl"><Iconz name={benefit.icon} /></span>
+                  <span className="text-[#5664f5] text-2xl">
+                    <Iconz name={benefit.icon} />
+                  </span>
                   <span className="text-black">{benefit.name} </span>
                   <input
                     type="text"
                     placeholder={`$${benefit.amount}`}
-                    className='bg-white text-[#5664f5] border border-solid
-                      border-[#5664f5] rounded-lg py-0 px-2 w-full'
+                    className="bg-white text-[#5664f5] border border-solid
+                      border-[#5664f5] rounded-lg py-0 px-2 w-full"
                   />
                   {/* <span className="benefit-amount">
                     ${benefit.amount} / {benefit.unit}
@@ -115,80 +122,86 @@ const [benefits, setBenefits] = useState([]);
             ))}
           </ul>
 
-          <h2 className="text-5xl font-bold text-black mb-2 flex flex-col mt-4">Job Details</h2>
-          <div className="flex flex-col items-center align-center gap-2 w-full max-w-sm">
-            <label>
-              Employment
-              <select className="text-[13px] text-[#5664f5] mx-3 py-0 px-2
+          <h2 className="text-5xl font-bold text-black mb-2 flex flex-col mt-4">
+            Job Details
+          </h2>
+          <div className="flex flex-col items-center align-center">
+            <div className="flex flex-col items-right align-center gap-0 w-full max-w-sm">
+              <label>
+                Employment
+                <select
+                  className="text-[13px] text-[#5664f5] mx-3 py-0 px-2
                   border border-solid bg-white
                   rounded-lg border-[#5664f5]
                   font-lexendDeca leading-[35.333335876464844px]
                   focus:outline-none 
                   focus:text-purple-800 cursor-pointer"
-                value={formData.jobDetails.employment}
-                onChange={(e) =>
-                  handleJobChange("employment ", e.target.value)
-                }
-              >
-                <option>Full-time</option>
-                <option>Part-time</option>
-                <option>Contract</option>
-              </select>
-            </label>
+                  value={formData.jobDetails.employment}
+                  onChange={(e) =>
+                    handleJobChange("employment ", e.target.value)
+                  }
+                >
+                  <option>Full-time</option>
+                  <option>Part-time</option>
+                  <option>Contract</option>
+                </select>
+              </label>
+              <label>
+                Hours per Week
+                <select
+                  className="text-[13px] text-[#5664f5] mx-3 py-0 px-2
+                  border border-solid bg-white
+                  rounded-lg border-[#5664f5]
+                  font-lexendDeca leading-[35.333335876464844px]
+                  focus:outline-none 
+                  focus:text-purple-800 cursor-pointer"
+                  value={formData.jobDetails.hours}
+                  onChange={(e) =>
+                    handleJobChange("hours", Number(e.target.value))
+                  }
+                >
+                  {[10, 20, 30, 40].map((h) => (
+                    <option key={h} value={h}>
+                      {h}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <label>
+                Pay Type
+                <select
+                  className="text-[13px] text-[#5664f5] mx-3 py-0 px-2
+                  border border-solid bg-white
+                  rounded-lg border-[#5664f5]
+                  font-lexendDeca leading-[35.333335876464844px]
+                  focus:outline-none 
+                  focus:text-purple-800 cursor-pointer"
+                  value={formData.jobDetails.payType}
+                  onChange={(e) => handleJobChange("payType", e.target.value)}
+                >
+                  <option>Hourly</option>
+                  <option>Salary</option>
+                </select>
+              </label>
 
-            <label>
-              Hours per Week  
-              <select className="text-[13px] text-[#5664f5] mx-3 py-0 px-2
+              <label>
+                Pay Rate
+                <select
+                  className="text-[13px] text-[#5664f5] mx-3 py-0 px-2
                   border border-solid bg-white
                   rounded-lg border-[#5664f5]
                   font-lexendDeca leading-[35.333335876464844px]
                   focus:outline-none 
                   focus:text-purple-800 cursor-pointer"
-                value={ formData.jobDetails.hours}
-                onChange={(e) =>
-                  handleJobChange("hours", Number(e.target.value))
-                }
-              >
-                {[10, 20, 30, 40].map((h) => (
-                  <option key={h} value={h}>
-                    {h}
-                  </option>
-                ))}
-              </select>
-            </label>
-
-            <label>
-              Pay Type
-              <select className="text-[13px] text-[#5664f5] mx-3 py-0 px-2
-                  border border-solid bg-white
-                  rounded-lg border-[#5664f5]
-                  font-lexendDeca leading-[35.333335876464844px]
-                  focus:outline-none 
-                  focus:text-purple-800 cursor-pointer"
-                value={formData.jobDetails.payType}
-                onChange={(e) => handleJobChange("payType", e.target.value)}
-              >
-                <option>Hourly</option>
-                <option>Salary</option>
-              </select>
-            </label>
-
-            <label>
-              Pay Rate
-              <select className="text-[13px] text-[#5664f5] mx-3 py-0 px-2
-                  border border-solid bg-white
-                  rounded-lg border-[#5664f5]
-                  font-lexendDeca leading-[35.333335876464844px]
-                  focus:outline-none 
-                  focus:text-purple-800 cursor-pointer"
-                value={formData.jobDetails.payRate}
-                onChange={(e) => handleJobChange("payRate", e.target.value)}
-              >
-                <option>Weekly</option>
-                <option>Bi-Weekly</option>
-                <option>Monthly</option>
-              </select>
-            </label>
+                  value={formData.jobDetails.payRate}
+                  onChange={(e) => handleJobChange("payRate", e.target.value)}
+                >
+                  <option>Weekly</option>
+                  <option>Bi-Weekly</option>
+                  <option>Monthly</option>
+                </select>
+              </label>
+            </div>
           </div>
 
           <button
