f44ba803702e26e05e144aa45d798d8f1e277964
Pedro Echavarria <pechavarria62@gmail.com>
2026-04-07 20:07:23 -0400

    woking on city and state file

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-// import CityState from "./pages/CityState";
-import ZipCode from "./pages/ZipCode";
+import CityState from "./pages/CityState";
+// import ZipCode from "./pages/ZipCode";
 import Results from "./pages/Results";
 import "./App.css";
 
@@ -15,8 +15,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/dashboard" element={<ZipCode />} />
-        {/* <Route path="/dashboard" element={<CityState />} /> */}
+        {/* <Route path="/dashboard" element={<ZipCode />} /> */}
+        <Route path="/dashboard" element={<CityState />} />
         <Route path="/results" element={<Results />} />
       </Routes>
     </Router>
